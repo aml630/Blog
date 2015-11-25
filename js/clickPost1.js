36c45112522b854eb1ecd61ad93f599c27ae85d9
alex <alex.m.larson@gmail.com>
2015-11-25 09:38:49 -0800

    article written, changes made

--- js/clickPost1.js
+++ js/clickPost1.js
@@ -2,7 +2,7 @@
 
 
 
-document.write("<img class = 'featureImage' src = 'http://longfengdazhan.com/wp-content/uploads/2014/11/%E7%BA%A2%E8%8D%AF%E4%B8%B8.jpg' >                         ")
-document.write("    <a href = 'Post1.html'> <h1 class = 'title'>Going Rogue</h1> </a>   ")
+document.write("<img class = 'featureImage' src = 'https://pixabay.com/static/uploads/photo/2015/08/13/01/00/keyboard-886462_640.jpg' >                         ")
+document.write("    <a href = 'Post1.html'> <h1 class = 'title'>Find Coding Bootcamps In Your State</h1> </a>   ")
 document.write("    <h2 class = 'date'>November 16th 2015</h2>      ")
-document.write("   <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis quis tinciduntleo. Sed sed leo augue. Nunc luctus, nibh sit amet consequat tempor, ligula auue ultrices leo, a mattis purus ante vel mi. Praesent justo mauris, hendreri</p>       ")
+document.write("   <p>Click where you live to get a list of all the bootcamps near you.</p>       ")
--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,59 @@ $(document).ready(function (){
 
 
 
+$(".go").click(function () {
+
+
+
+
+
+var language = $("#languages").val();
+
+if (language == "None") {
+//  $(".allBootcamps div").hide();
+}
+
+
+else if (language == "Ruby") {
+  $(".bootcamps div").hide();
+  $("#ADA").show();
+  $("#anyoneCanLearn").show();
+  $("#appAcademy").show();
+  $("#anyoneCanLearn").show();
+  $("#apprentice").show();
+}
+else if (language == "Javascript") {
+  $(".bootcamps div").hide();
+  $(".allBootcamps div").show();
+
+}
+else if (language == "Angular") {
+  $(".allBootcamps div").hide();
+  $("#anyoneCanLearn").show();
+
+}
+});
+
+
+
+
+$(".showAll").click(function () {
+$(".allBootcamps div").show();
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
 $("#me").hover(function () {
 $(this).toggleClass("imagehover")
 
@@ -68,6 +121,232 @@ if ($(this).scrollTop() > 1){
 
 
 /* End Sticky Header Code */
+
+
+$('#map').usmap({
+  // The click action
+  click: function(event, data) {
+    $('#clicked-state')
+      .text('You clicked: '+data.name)
+      .parent().effect('highlight', {color: '#C7F464'}, 2000);
+  }
+});
+
+$('#map').click(function() {
+
+  var state = $("#clicked-state").text();
+
+  if (state == "You clicked: OR") {
+    $(".allBootcamps div").hide();
+    $("#epicodus").show();
+    $("#apprentice").show();
+    $("#portlandCodeGuild").show();
+    $("#theTechAcademy").show();
+
+
+
+  }
+
+  if (state == "You clicked: WA") {
+    $(".allBootcamps div").hide();
+    $("#ADA").show();
+    $("#coderCamps").show();
+    $("#codingDojo").show();
+    $("#codingHouse").show();
+    $("#fireBootCamp").show();
+    $("#galvonize").show();
+    $("#generalAssembly").show();
+
+
+
+  }
+  if (state == "You clicked: CA") {
+    $(".allBootcamps div").hide();
+    $("#angelHack").show();
+    $("#anyoneCanLearn").show();
+    $("#appAcademy").show();
+    $("#apprentice").show();
+    $("#bigNerdRanch").show();
+    $("#coderCamps").show();
+    $("#codingDojo").show();
+    $("#devBootcamp").show();
+    $("#geekwiseAcademy").show();
+    $("#galvonize").show();
+    $("#generalAssembly").show();
+    $("#hackReactor").show();
+    $("#hackbrightAcademy").show();
+    $("#learnTechLabs").show();
+    $("#learningFuze").show();
+    $("#makeSchool").show();
+    $("#makerSquare").show();
+    $("#mobileMakersAcademy").show();
+    $("#rocketU").show();
+    $("#sabio").show();
+
+
+
+
+
+  }
+  if (state == "You clicked: IL") {
+    $(".allBootcamps div").hide();
+    $("#anyoneCanLearn").show();
+    $("#codeCore").show();
+    $("#designation").show();
+    $("#devBootcamp").show();
+    $("#mobileMakersAcademy").show();
+    $("#starterLeague").show();
+    $("#startupInstitute").show();
+
+
+
+  }
+  if (state == "You clicked: MI") {
+    $(".allBootcamps div").hide();
+    $("#grandCircus").show();
+  }
+
+  if (state == "You clicked: NY") {
+    $(".allBootcamps div").hide();
+    $("#appAcademy").show();
+    $("#apprentice").show();
+    $("#brainStation").show();
+    $("#byteAcademy").show();
+    $("#devBootcamp").show();
+    $("#falitronSchool").show();
+    $("#fullstackAcademy").show();
+    $("#makeSchool").show();
+    $("#newYorkCodeDesignAcademy").show();
+    $("#recurseCenter").show();
+    $("#shillingtonSchool").show();
+    $("#startupInstitute").show();
+
+
+
+  }
+  if (state == "You clicked: TX") {
+    $(".allBootcamps div").hide();
+    $("#apprentice").show();
+    $("#coderCamps").show();
+    $("#coderVox").show();
+    $("#codeUp").show();
+    $("#devMountain").show();
+    $("#theIronYard").show();
+    $("#techTalentSouth").show();
+
+
+}
+    if (state == "You clicked: TN") {
+      $(".allBootcamps div").hide();
+      $("#nashvilleSoftwareSchool").show();
+
+
+  }
+  if (state == "You clicked: OH") {
+    $(".allBootcamps div").hide();
+    $("#softwareGuild").show();
+    $("#weCanCodeIt").show();
+
+
+}
+  if (state == "You clicked: MA") {
+    $(".allBootcamps div").hide();
+    $("#apprentice").show();
+    $("#generalAssembly").show();
+    $("#launchAcademy").show();
+
+
+  }
+  if (state == "You clicked: PA") {
+    $(".allBootcamps div").hide();
+    $("#apprentice").show();
+  }
+  if (state == "You clicked: CO") {
+    $(".allBootcamps div").hide();
+    $("#apprentice").show();
+    $("#davinciCoders").show();
+    $("#galvanize").show();
+    $("#reFactor").show();
+    $("#turingSchool").show();
+
+
+  }
+  if (state == "You clicked: NC") {
+    $(".allBootcamps div").hide();
+    $("#apprentice").show();
+    $("#coderFoundry").show();
+    $("#craftsmenshipAcademy").show();
+    $("#theIronYard").show();
+    $("#techTalentSouth").show();
+    $("#turnToTech").show();
+
+
+
+
+  }
+  if (state == "You clicked: MD") {
+    $(".allBootcamps div").hide();
+    $("#betamore").show();
+  }
+  if (state == "You clicked: GA") {
+    $(".allBootcamps div").hide();
+    $("#bigNerdRanch").show();
+    $("#theIronYard").show();
+    $("#techTalentSouth").show();
+
+  }
+  if (state == "You clicked: UT") {
+    $(".allBootcamps div").hide();
+    $("#CodingCampus").show();
+    $("#devMountain").show();
+    $("#devPointLabs").show();
+
+
+  }
+  if (state == "You clicked: NM") {
+    $(".allBootcamps div").hide();
+    $("#deepDiveCoding").show();
+  }
+  if (state == "You clicked: HI") {
+    $(".allBootcamps div").hide();
+    $("#devLeague").show();
+  }
+  if (state == "You clicked: MO") {
+    $(".allBootcamps div").hide();
+    $("#disruption").show();
+  }
+  if (state == "You clicked: NV") {
+    $(".allBootcamps div").hide();
+    $("#theIronYard").show();
+  }
+  if (state == "You clicked: FL") {
+    $(".allBootcamps div").hide();
+    $("#ironHack").show();
+    $("#Wyncode").show();
+
+  }
+
+  if (state == "You clicked: AZ") {
+    $(".allBootcamps div").hide();
+    $("#rockItBootcamp").show();
+  }
+
+  if (state == "You clicked: VA") {
+    $(".allBootcamps div").hide();
+    $("#techTalentSouth").show();
+  }
+
+
+})
+
+
+
+//$("div:contains('OR')").css("text-decoration"; "underline")
+
+
+
+
+
 
 
 
